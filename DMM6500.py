e69b609f644c4de13fe21d94dcc139a93ff79de5
kaspervn <kasper.dev@fastmail.com>
2021-01-06 17:01:40 +0100

    initial version of the controller class

---
+++ DMM6500.py
@@ -0,0 +1,56 @@
+import pyvisa as visa
+from enum import Enum
+
+
+class Function(Enum):
+    DC_VOLTAGE = 'VOLT:DC'
+    AC_VOLTAGE = 'VOLT:AC'
+    DC_CURRENT = 'CURR:DC'
+    AC_CURRENT = 'CURR:AC'
+    RESISTANCE = 'RES'
+    FOUR_WIRE_RESISTANCE = 'FRES'
+    DIODE = 'DIO'
+    CAPACITANCE = 'CAP'
+    TEMPERATURE = 'TEMP'
+    CONTINUITY = 'CON'
+    FREQUENCY = 'FREQ'
+    PERIOD = 'PER'
+    VOLTAGE_RATIO = 'VOLT:RAT'
+
+
+class DMM6500:
+
+    def __init__(self, resource: visa.Resource):
+        self.r = resource
+        self.last_selected_function = None
+
+    def reset(self):
+        self.r.write('*RST')
+
+    def set_function(self, func: Function):
+        self.r.write(f':SENS:FUNC "{_scpi_func_name(func)}"')
+        self.last_selected_function = func
+
+    def set_range(self, max_expected_value: float):
+        self.r.write(self._sense_prefix() + f'RANG {max_expected_value}')
+
+    def set_auto_range(self, yes=True):
+        self.r.write(self._sense_prefix() + f'RANG:AUTO {1 if yes else 0}')
+
+    def set_auto_zero(self, yes=True):
+        self.r.write(self._sense_prefix() + f'AZER {1 if yes else 0}')
+
+    def set_nplc(self, nplc: float):
+        self.r.write(self._sense_prefix() + f'NPLC {nplc}')
+
+    def measure(self):
+        return self.r.query(':MEAS?')
+
+    def _sense_prefix(self):
+        if self.last_selected_function is None:
+            raise Exception("No function selected yet")
+        return f':SENS:{_scpi_func_name(self.last_selected_function)}:'
+
+
+def _scpi_func_name(func: Function):
+    return func.value
--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # py-dmm6500
 
 Helper functions to communicate with a Keithley DMM6500 multimeter via pyvisa
+
+Note: Be sure to set the multimeter in SCPI communication mode
+
+Example usage:
+````
+import pyvisa as visa
+from DMM6500 import DMM6500
+
+rm = visa.ResourceManager()
+mm = DMM6500(rm.open_resource('USB::0x05e6::0x6500::*******::INSTR'))
+mm.reset()
+mm.set_function(Function.DC_VOLTAGE)
+mm.set_auto_range()
+mm.set_nplc(2)
+
+print(mm.measure())
+````
